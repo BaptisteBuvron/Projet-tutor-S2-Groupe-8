2764024a7ecef83ff14b45ba34cb0240e52606b7
Baptiste Buvron <baptiste.buvron@gmail.com>
2021-02-14 12:14:22 +0100

    add hello world javafx

--- src/main.java
+++ src/main.java
@@ -1,5 +1,15 @@
-public class main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
     public static void main(String args[]){
         System.out.println("test 3 par eclipse");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("hello world");
+        stage.show();
     }
 }
