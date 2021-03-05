26ede539bc6215cf91399c8f2b4421c554d336a6
Baptiste Buvron <baptiste.buvron@gmail.com>
2021-03-05 14:35:46 +0100

    Convertir une ressource en tableau de bytes, stockage dans un objet exercice.
    Conversion d'un tableau de bytes en fichier.

--- src/Application/Exercice.java
+++ src/Application/Exercice.java
@@ -5,6 +5,56 @@ import javafx.scene.media.Media;
 public class Exercice {
     private String name;
     private String instruction;
-    private Media media;
+    private byte[] fileByte;
+    private String fileName;
+    private byte[] imageBytes;
+    private String imageName;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public byte[] getFileByte() {
+        return fileByte;
+    }
+
+    public void setFileByte(byte[] fileByte) {
+        this.fileByte = fileByte;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public byte[] getImageBytes() {
+        return imageBytes;
+    }
+
+    public void setImageBytes(byte[] imageBytes) {
+        this.imageBytes = imageBytes;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
 }
--- src/Application/MainController.java
+++ src/Application/MainController.java
@@ -10,10 +10,16 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
-
 
 
 public class MainController implements Initializable {
@@ -33,14 +39,14 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    public void chooseFileAction(ActionEvent e){
+    public void chooseFileAction(ActionEvent e) throws IOException {
         //Si un media est en lecture on le stop.
-        if (mediaPlayer != null){
+        if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
 
         //Si une image est affiché on l'a supprime
-        if (imageView.getImage() != null){
+        if (imageView.getImage() != null) {
             imageView.setImage(null);
         }
 
@@ -66,12 +72,46 @@ public class MainController implements Initializable {
             String fileName = selectedFile.getName();
 
             //extension du fichier
-            String extensionFile = fileName.substring(fileName.lastIndexOf(".")+1);
+            String extensionFile = fileName.substring(fileName.lastIndexOf(".") + 1);
 
-            //Si le fichier est une vidéo
-            if (extensionFile.equals("mp3")){
+            Main.exercice.setFileName(fileName);
+
+            //Convertir la vidéo en bytes
+            Main.exercice.setFileByte(Files.readAllBytes(selectedFile.toPath()));
+
+            //Créer le dossier dans les documents s'il n'existe pas.
+            /*File theDir = new File(FileSystemView.getFileSystemView().getDefaultDirectory().getPath()+"\\ProjetTutoré");
+            if (!theDir.exists()){
+                theDir.mkdirs();
+            }*/
+
+            //Ecrire le fichier dans le dossier dans les documents
+            /*try(FileOutputStream stream = new FileOutputStream(FileSystemView.getFileSystemView().getDefaultDirectory().getPath()+"\\ProjetTutoré\\"+ Main.exercice.getFileName())){
+                stream.write(Main.exercice.getFileByte());
+            }*/
+
+            //Si le fichier est une musique
+            if (extensionFile.equals("mp3")) {
                 //On ajoute une image de fond
-                Image image = new Image("https://img.lignes-formations.com/wp-content/uploads/sites/45/2019/05/formation-reportage-photo-niveau-1.jpg");
+                FileChooser fileChooserImage = new FileChooser();
+                fileChooserImage.getExtensionFilters().addAll(
+                        new FileChooser.ExtensionFilter("Image jpg", "*.jpg"),
+                        new FileChooser.ExtensionFilter("Image png", "*.png"),
+                        new FileChooser.ExtensionFilter("Image jpeg", "*.jpeg")
+                );
+                fileChooserImage.setTitle("Choisir une image d'illustration");
+                File imageSelected = null;
+                while (imageSelected == null) {
+                    imageSelected = fileChooserImage.showOpenDialog(Main.stage);
+                }
+
+                //Récupérer le nom de l'image
+                Main.exercice.setImageName(imageSelected.getName());
+
+                //Convertir l'image en bytes
+                Main.exercice.setImageBytes(Files.readAllBytes(imageSelected.toPath()));
+
+                Image image = new Image(imageSelected.toURI().toString());
                 imageView.setImage(image);
             }
 
@@ -88,19 +128,11 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    public void nextSceneButton(ActionEvent e){
+    public void nextSceneButton(ActionEvent e) {
         Main.stage.setTitle("deuxième page");
         Main.stage.setScene(Main.scene2);
 
     }
-
-
-
-
-
-
-
-
 
 
 }
